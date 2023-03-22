f09fe2d433e48d67617ae63bab9c4a57616885de
TonyDev92 <tonydeveloper92@gmail.com>
2023-03-22 19:17:25 +0100

    Schema gamesApi v1

---
+++ src/api/controllers/games.controllers.js
@@ -0,0 +1,40 @@
+
+
+
+const getGame = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+const getGameById = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+const putGame = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+const postGame = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+const deleteGame = async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
---
+++ src/api/models/games.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema(
+    {
+        title: {type:String , required: true},
+        genre: {type:String , required: true},
+        releaseDate: {type:String , required: true},
+        developer: {type:String, required: true},
+        publisher: {type:String, required:true},   
+        image: {type:String , required: false}
+    },{
+        timestamps:true
+    }
+)
+
+const Game = mongoose.model('game', gameSchema);
+module.exports = Game;
