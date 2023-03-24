e740b99db6178963100b3d51787f320029d003d1
DhervasQ <danielhervasqueralt@gmail.com>
2023-03-24 13:16:26 +0100

    Incluimos paginacion

--- src/api/controllers/games.controllers.js
+++ src/api/controllers/games.controllers.js
@@ -1,71 +1,109 @@
-const Game = require('../models/games.model');
+const Game = require("../models/games.model");
 
 //GET
-const getGame = async(req,res) => {
-    try {
+const getGame = async (req, res) => {
+  try {
+    let { page, limit } = req.query;
+    const numGames = await Game.countDocuments();
+    limit = limit ? parseInt(limit) :5;
+    if (page && !isNaN(parseInt(page))) {
+      page = parseInt(page);
+      let numPages =
+        numGames % limit > 0 ? numGames / limit + 1 : numGames / limit;
 
-        const allGames = await Game.find();
+      if (page > numPages) page = numPages;
 
-        return res.status(200).json(allGames);
+      if (page < 1) page = 1;
 
-    } catch (error) {
-        res.status(500).json(error)
+      const skip = (page - 1) * limit; 
+
+      const games = await Game.find().skip(skip).limit(limit);
+      return res.status(200).json({
+        info: {
+          numTotal: numGames,
+          page: page,
+          limit: limit,
+          nextPage:
+            numPages >= page + 1
+              ? `/games?page=${page + 1}&limit=${limit}`
+              : null,
+          prevPage: page != 1 ? `/games?page=${page - 1}&limit=${limit}` : null,
+        },
+        results: games,
+      });
+    } else {
+      const games = await Game.find().limit(limit);
+      return res.status(200).json({
+        info: {
+          numTotal: numGames,
+          page: 1,
+          limit: limit,
+          nextPage: numGames > limit ? `/games?page=2&limit=${limit}` : null,
+          prevPage: null,
+        },
+        results: games,
+      });
     }
-}
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+//   limite
+const getGameById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const game = await Game.findById(id);
 
-const getGameById = async(req,res) => {
-    try {
-
-        const { id } = req.params;
-        const game = await Game.findById(id);
-
-        !game ? res.status(404).json({"message": "Game not found."}) :  res.status(200).json(game);
-
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+    !game
+      ? res.status(404).json({ message: "Game not found." })
+      : res.status(200).json(game);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 //PUT
-const putGame = async(req,res) => {
-    try {
+const putGame = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const putGame = new Game(req.body);
+    putGame._id = id;
+    const updatedGame = await Game.findByIdAndUpdate(id, putGame, {
+      new: true,
+    });
 
-        const { id } = req.params;
-        const putGame = new Game(req.body);
-        putGame._id = id;
-        const updatedGame = await Game.findByIdAndUpdate(id, putGame, {new: true});
-
-        !updatedGame ? res.status(404).json({"message": "Game not found to be updated."}) :  res.status(200).json(updatedGame);
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+    !updatedGame
+      ? res.status(404).json({ message: "Game not found to be updated." })
+      : res.status(200).json(updatedGame);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 //POST
-const postGame = async(req,res) => {
-    try {
+const postGame = async (req, res) => {
+  try {
+    const newGame = new Game(req.body);
+    const createdGame = await newGame.save();
 
-        const newGame = new Game(req.body);
-        const createdGame = await newGame.save();
-
-        return res.status(200).json(createdGame);
-
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+    return res.status(200).json(createdGame);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 //DELETE
-const deleteGame = async(req,res) => {
-    try {
-        const { id } = req.params;
-        const deletedGame = await Game.findByIdAndDelete(id);
+const deleteGame = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedGame = await Game.findByIdAndDelete(id);
 
-        !deletedGame ? res.status(404).json({"message": "Game not found to be deleted."}) :  res.status(200).json(deletedGame);
+    !deletedGame
+      ? res.status(404).json({ message: "Game not found to be deleted." })
+      : res.status(200).json(deletedGame);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
-
-module.exports = {getGame, putGame, getGameById, postGame, deleteGame};
+module.exports = { getGame, putGame, getGameById, postGame, deleteGame };
