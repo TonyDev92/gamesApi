01903fa399e04f3e1bff213d9617ffa187f3309a
TonyDev92 <tonydeveloper92@gmail.com>
2023-03-25 10:46:32 +0100

    Schema gamesApi v16

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cloudinary = require('cloudinary').v2;
-// const cors = require('cors');
+const cors = require('cors');
 const {connect} = require('./src/utils/database');
 const routerGames = require('./src/api/routes/games.routes');
 const routerPlatforms = require('./src/api/routes/platform.routes');
@@ -18,10 +18,10 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_SECRET
 });
 
-// app.use(cors({
-//     origin: ["http://localhost:3000", "http://localhost:4200", "http://localhost:5000", "http://localhost:5500", "https://gamesapi.vercel.app/"],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:4200", "http://localhost:5000", "http://localhost:5500", "https://gamesapi.vercel.app/"],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -30,8 +30,8 @@ app.use('/games', routerGames);
 app.use('/platforms', routerPlatforms);
 app.use('/user', routerUser);
 
-// app.use('*', (req, res) => {
-//     res.status(404).json('Route not found');
-// });
+app.use('*', (req, res) => {
+    res.status(404).json('Route not found');
+});
 
 app.listen(PORT, () => console.log(`listening on: http://localhost:${PORT}`));
