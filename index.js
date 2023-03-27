e3a08f8542c1a01fc5104d0aeee54d284160f2e6
TonyDev92 <tonydeveloper92@gmail.com>
2023-03-27 09:47:09 +0200

    Schema gamesApi v20

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ cloudinary.config({
 });
 // ["http://localhost:3000", "http://localhost:4200", "http://localhost:5000", "http://localhost:5500"],
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:4200", "http://localhost:5000", "http://localhost:5500", "https://gamesapi.vercel.app/"],
+    origin: "*",
     credentials: true
 }));
 
